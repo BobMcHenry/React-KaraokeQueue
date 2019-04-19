bcb53fd34f36a977b2f99fd057374f492db6cb1d
Bob M <BobMcHenryVideo@gmail.com>
2019-04-18 20:51:57 -0700

    Updates

---
+++ precache-manifest.ff27ab5647e60f60efe42f717371ef46.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b13de62b7e7a13b04ba3",
+    "url": "/React-KaraokeQueue/static/js/runtime~main.1c66d1cf.js"
+  },
+  {
+    "revision": "695d94cdb9d1012c3351",
+    "url": "/React-KaraokeQueue/static/js/main.4c5016d3.chunk.js"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/React-KaraokeQueue/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "695d94cdb9d1012c3351",
+    "url": "/React-KaraokeQueue/static/css/main.7554037d.chunk.css"
+  },
+  {
+    "revision": "d4f5fc63e95437fd533b7f9b6a9970f4",
+    "url": "/React-KaraokeQueue/index.html"
+  }
+];
