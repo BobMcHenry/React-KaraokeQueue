466b7b6c9a6fb76ed3731d17782824688947a556
bobmchenry <Bungle666!>
2019-05-03 23:53:52 -0700

    Add Jest/Enzyme unit tests for App Component

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+import { shallow, mount, render } from 'enzyme';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+import App from './App';
+import SearchPanel from './components/SearchPanel';
+
+configure({ adapter: new Adapter() });
+
+describe('App component', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('contains a search panel component', () => {
+    const wrapper = mount(<App />);
+    expect(wrapper.children().exists( 'SearchPanel' )).toBe(true);
+  });
+
+  it('contains a queue panel component', () => {
+    const wrapper = mount(<App />);
+    expect(wrapper.children().exists( 'QueuePanel' )).toBe(true);
+  });
 });
